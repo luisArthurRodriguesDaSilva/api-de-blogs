a54546106f14ed92d296c1f2b768418e1920bbd1
luis arthur <luisarthurlards03@gmail.com>
2022-12-04 15:54:03 -0300

    req 7

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+const CategoryModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      },
+    name:DataTypes.STRING,
+  },
+  {
+    sequelize,
+    underscored:true,
+    timestamps: false,
+    tableName:'categories',
+  }
+  );
+
+  return Category;
+};
+
+module.exports = CategoryModel;
