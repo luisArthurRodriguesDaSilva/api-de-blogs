5757d62bca81310b10244133a95621ce228d200d
luis arthur <luisarthurlards03@gmail.com>
2022-12-05 15:02:20 -0300

    removendo transaction para teste

--- src/services/post.services.js
+++ src/services/post.services.js
@@ -1,12 +1,6 @@
-const Sequelize = require('sequelize');
-const config = require('../config/config');
-
-const env = process.env.NODE_ENV || 'development';
-const sequelize = new Sequelize(config[env]);
-
 const { BlogPost, PostCategory } = require('../models');
 
-const addBlogPost = async (transaction, userId, actDate, { title, content }) => {
+const addBlogPost = async (userId, actDate, { title, content }) => {
   const newPost = (await BlogPost.create(
     {
       userId,
@@ -15,17 +9,16 @@ const addBlogPost = async (transaction, userId, actDate, { title, content }) => 
       title,
       content,
     },
-    { transaction },
   )).dataValues;
   return newPost.id;
 };
 
-const addPostCategories = async (transaction, postId, post) => {
+const addPostCategories = async (postId, post) => {
   Promise.all(post.categoryIds.map((categoryId) => (
     PostCategory.create({
       postId,
       categoryId,
-    }, { transaction })
+    })
     )));
 };
 
@@ -40,15 +33,13 @@ const postApost = async (post, user) => {
   const userId = user.data;
   console.log('userId', userId);
   const actDate = new Date();
-  const t = await sequelize.transaction();
+  // const t = await sequelize.transaction();
   try {
-    const newPostId = await addBlogPost(t, userId, actDate, post);
-    await addPostCategories(t, newPostId, post);
-    t.commit();
+    const newPostId = await addBlogPost(userId, actDate, post);
+    await addPostCategories(newPostId, post);
     const newPost = await getApost(newPostId);
     return newPost;
   } catch (err) {
-    await t.rollback();
     return { error: true, message: err.message };
   }
 };
